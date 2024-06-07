93c365684b0a2b1dc7c4a14454c51ca608ee5c06
LexJarl <lolasrusas30@gmail.com>
2024-06-07 08:46:14 -0500

    Update 07.js
    Resuelto

--- C5 Fundamentos Javascript/ejercicios/07.js
+++ C5 Fundamentos Javascript/ejercicios/07.js
@@ -2,7 +2,7 @@ function esTipoDato(valor) {
   // La función recibe un argumento llamado valor.
   // Retorna el tipo de dato de este valor.
   // Por ejemplo: "string", "number", "boolean", "object", etc.
-  // Tu código:
+  // Tu código: number
 }
 
 module.exports = esTipoDato;
